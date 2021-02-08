864531ec1f323d3457ead78edc54a875b7c9e972
stevenduq <44072409+stevenduq@users.noreply.github.com>
2021-02-08 17:14:31 -0500

    petite erreur corrigé et changement des paramêtre d'entré

--- 8INF852TP.cpp
+++ 8INF852TP.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 
-int main(int argc, char* argv[]) 
-{	
+int main(int argc, char* argv[])
+{
 	clock_t	Start, End;	//Déclaration de variable afin de calculer le temps écoulé
 	double Elapsed = 0;	//Variable servant à calculer le temps écoulé (Différence entre End et Start
 	double	dTheBestFitness = 100000;	//Fitness de la meilleure solution
-	/*char meth = argv[3][0];
-
+	char meth = argv[3][0];
+	/*
 	char wait;
 	switch (meth)
 	{
@@ -41,10 +41,10 @@ int main(int argc, char* argv[])
 	/*cout << "Quelle methode voulez vous usiliser ? \n 1 -> Descente  \n 2 -> Recuit Simule  \n 3 -> Recherche Taboue \n";
 	char result;
 	cin >> result;*/
-		
-	
 
-	
+
+
+
 	// argv[1] exécutions de la génération aléatoire
 	for (int j = 0; j < atoi(argv[1]); j++)
 	{
@@ -52,26 +52,42 @@ int main(int argc, char* argv[])
 		pSolution = new SMSSDTSolution(LeProb->getN(), argv[3][0]);
 		SMSSDTSolution	Smeilleur = *pSolution;	//Sauvegarde de la meilleure solution
 		pSolution->descenteVoisinage(LeProb->getN());
-		for (int i = 0; i < pSolution->voisinage.size(); i++)
+		switch (meth)
 		{
-			pSolution->Solution = pSolution->voisinage.at(i);
-			//int test = pSolution->voisinage.size();
-			vector<int> wala = pSolution->Solution;
-			//pSolution = new SMSSDTSolution(LeProb->getN(), true);	//Une solution aléatoire
-			vector<int> wala2 = Smeilleur.Solution;
-			Tools::Evaluer(LeProb, *pSolution);	//Évaluer la solution
-			double abc = pSolution->getObj();
-			double abcd = Smeilleur.getObj();
-			if (pSolution->getObj() < dTheBestFitness) // Si améliore meilleure solution, la garder
+		case '1':
+			//Descente
+			for (int i = 0; i < pSolution->voisinage.size(); i++)
 			{
-				Smeilleur = *pSolution;
-				dTheBestFitness = Smeilleur.getObj();
-				i = 0;
-				pSolution->descenteVoisinage(LeProb->getN());
+				pSolution->Solution = pSolution->voisinage.at(i);
+				//int test = pSolution->voisinage.size();
+				vector<int> wala = pSolution->Solution;
+				//pSolution = new SMSSDTSolution(LeProb->getN(), true);	//Une solution aléatoire
+				vector<int> wala2 = Smeilleur.Solution;
+				Tools::Evaluer(LeProb, *pSolution);	//Évaluer la solution
+				double abc = pSolution->getObj();
+				double abcd = Smeilleur.getObj();
+				if (pSolution->getObj() < dTheBestFitness) // Si améliore meilleure solution, la garder
+				{
+					Smeilleur = *pSolution;
+					dTheBestFitness = Smeilleur.getObj();
+					i = 0;
+					pSolution->descenteVoisinage(LeProb->getN());
+				}
 			}
-			//else 
-			//delete pSolution;
+			break;
+		case '2':
+			//Taboue
+
+			break;
+		case '3':
+			//Recuit
+			break;
+		default:
+			cout << "Mauvaise entre";
+			break;
 		}
+
+
 		delete pSolution;
 		End = clock(); // Arrêter le clock
 		Elapsed = (double(End - Start)) / CLOCKS_PER_SEC;	//Calculer le temps écoulé
@@ -79,7 +95,7 @@ int main(int argc, char* argv[])
 		dTheBestFitness = 100000;
 
 	}
-	
+
 
 	return 0;
 
